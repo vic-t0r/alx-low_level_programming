f967b316de1de2efdd95c50bcdb96c915a9c0f43
vic-t0r <owolex472@gmail.com>
2022-12-09 08:58:01 -0800

    low level programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
