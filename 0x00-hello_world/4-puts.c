e90c7707eff811753dd1dfcf86206aee07990d81
vic-t0r <owolex472@gmail.com>
2022-12-09 16:01:08 -0800

    low level programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- * main - Entry point
- *
- * Return: Always 0 (success)
+ *main - Entry point
+ *include main.c
+ *Return: Always 0 (success)
  */
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*main - Entry point
- *
+/*
+ * main - Entry point
+ *main is the universal function
  * Return always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-        return(0);
+	printf("with proper grammar, but the outcome is a piece of")
+	return (0);
 }
