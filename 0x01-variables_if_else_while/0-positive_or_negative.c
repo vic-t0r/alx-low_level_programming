d2db67b9349d4e89db258d5ba160a722b5417b81
vic-t0r <owolex472@gmail.com>
2022-12-10 18:07:44 -0800

    task 4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - main function to generate a random number
@@ -8,8 +10,8 @@
 int main(void)
 {
 	int n;
-	'srand' ("time"(0));
-	n = 'rand'() - 'RAND_MAX'/ 2;
+	srand (time(0));
+	n = rand() - RAND_MAX/ 2;
 
 	if (n > 0)
 	{
