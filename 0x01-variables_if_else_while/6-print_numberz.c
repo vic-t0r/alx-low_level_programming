301849ad81606b0eeafaaf853b8a407450ffab98
vic-t0r <owolex472@gmail.com>
2022-12-11 14:29:00 -0800

    task 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints number between 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0 ; i < 50; i++);
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
