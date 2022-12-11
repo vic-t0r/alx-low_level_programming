f42f919580ebba1cabbce8ee22a0b1bc9a9ab718
vic-t0r <owolex472@gmail.com>
2022-12-11 14:41:09 -0800

    task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- *main - Entry point
- *include main.c
- *Return: Always 0 (success)
+ * main - prints exactly 'Programming is like building a multilingual puzzle'
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
