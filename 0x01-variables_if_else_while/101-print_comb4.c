817ae5305c47e6d010e300fa44741013a76de3f7
vic-t0r <owolex472@gmail.com>
2022-12-11 13:15:45 -0800

    task 0

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,9 +17,9 @@ int main(void)
 			for (ones = '0'; ones <= '9'; ones++)
 			{
 				if (!((ones == tens) || 
-							(tens == hundreds) || 
-							(tens > ones) || 
-							(hundreds > tens)))
+(tens == hundreds) || 
+(tens > ones) || 
+(hundreds > tens)))
 				{
 					putchar(hundreds);
 					putchar(tens);
