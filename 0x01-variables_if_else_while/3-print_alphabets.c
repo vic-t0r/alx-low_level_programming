5ba2365f7b114f3cbebad072ea9cfbf3095a721c
vic-t0r <owolex472@gmail.com>
2022-12-11 13:40:45 -0800

    task 0

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,12 @@ int main(void)
 	int ch = 'a';
 	int CH = 'A';
 
-	while (ch <= 'z');
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	while (CH <= 'Z');
+	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
