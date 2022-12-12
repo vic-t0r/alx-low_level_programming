7a8d4ef15e54d2830e34096eac22f177868380fc
vic-t0r <owolex472@gmail.com>
2022-12-12 15:26:22 -0800

    task 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,11 +21,11 @@ int main(void)
 	}
 	else if (last == 0)
 	{
-		printf("last digit of %i is %i and is 0\n", n, last);
+		printf("last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	}
 	else if (last < 6)
 	{
-		printf("last digit of %i is %i and is less than 6 and not 0\n", n, last);
+		printf("last digit of %i is %i and is 0\n", n, last);
 	}
 	return (0);
 }
